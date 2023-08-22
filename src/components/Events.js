874eddd4701495b84844b88dd44cee455d99d0c9
Mohammed <86866415+BurhaneeDevelopers@users.noreply.github.com>
2023-08-22 19:06:13 +0530

    added dynamic events

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,24 +1,56 @@
 import React from "react";
 
 const Events = () => {
+  const events = [
+    {
+      name: "Murthy",
+      title: "This is title one",
+      desc: "Descripton is very short here",
+      color: "#F9C200",
+      date: ["12"],
+      time: "11:00",
+      month: "man",
+    },
+    {
+      name: "Taher",
+      title: "This is title two",
+      desc: "description is short but not more short here",
+      color: "#F97700",
+      date: "30",
+      time: "12:00",
+      month: "Meb",
+    },
+    {
+      name: "Huzefa",
+      title: "This is title three",
+      desc: "description is not s long but is is long here more than before",
+      color: "#00D9CC",
+      date: "21",
+      time: "1:00",
+      month: "March",
+    },
+  ];
   return (
     <div>
       <div class="col-span-12 rounded-3xl border border-stroke bg-white px-5 pt-7.5 pb-5 shadow dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8">
         <h2 class="mb-2 text-xl text-black-2 dark:text-white">Events</h2>
-        <div class="my-3 flex rounded-xl border bg-bodydark1">
-          <div class="flex flex-col items-center justify-center rounded-xl border-r-6 border-[#F9C200] bg-[#F9C200]/30 p-2 text-black">
-            20
-            <div class="text-[13px] font-semibold">Feb</div>
-            <div class="text-[13px]">4:00pm</div>
-          </div>
-          <div class="p-2 text-black">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Consectetur, enim. Odit numquam sit repellat, ullam sequi earum
-            maxime quae totam?
-          </div>
-        </div>
 
-        <div class="my-3 flex rounded-xl border bg-bodydark1">
+        {events.map((item, index) => {
+          return (
+            <div key={index} class="my-3 flex rounded-xl border items-center">
+              <div class="flex flex-col items-center justify-center rounded-xl border-r-6 border-[#F9C200] bg-[#F9C200]/30 p-2 text-black">
+                <span className="font-bold">{item.date}</span>
+                <div class="text-[13px] first-letter:capitalize font-semibold">
+                  {item.month}
+                </div>
+                <div class="text-[13px]">{item.time}</div>
+              </div>
+              <div class="p-2 text-black">{item.desc}</div>
+            </div>
+          );
+        })}
+
+        {/* <div class="my-3 flex rounded-xl border bg-bodydark1">
           <div class="flex flex-col items-center justify-center rounded-xl border-r-6 border-[#F97700] bg-[#F97700]/30 p-2 text-black">
             20
             <div class="text-[13px] font-semibold">Feb</div>
@@ -42,7 +74,7 @@ const Events = () => {
             Consectetur, enim. Odit numquam sit repellat, ullam sequi earum
             maxime quae totam?
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
